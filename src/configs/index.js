736eafa8402b1ca5c44d43d0d0e81bfa003cd304
dattruong88vn <dattt1.ho@mcredit.com.vn>
2022-10-14 15:15:33 +0700

    video 8: stacic file

---
+++ src/configs/index.js
@@ -0,0 +1,2 @@
+export { default as configViewEngine } from "./viewEngines.js";
+export { default as configStaticFolder } from "./staticFolder.js";
--- src/server.js
+++ src/server.js
@@ -1,13 +1,22 @@
 import express from "express";
-import configViewEngine from "./configs/viewEngines.mjs";
+import dotenv from "dotenv";
+
+import { configStaticFolder, configViewEngine } from "./configs/index.js";
+
+dotenv.config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
 configViewEngine(app);
+configStaticFolder(app);
 
 app.get("/", (req, res) => {
   res.render("index.ejs");
+});
+
+app.get("/test", (req, res) => {
+  res.render("test/index.ejs");
 });
 
 app.get("/about", (req, res) => {
